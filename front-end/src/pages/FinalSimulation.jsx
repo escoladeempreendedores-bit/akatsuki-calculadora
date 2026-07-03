c1e65c3850983e132c65bbe5d0797ecdc178aa15
Escola de Empreeendedores <escoladeempreendedoresoficial@gmail.com>
2026-07-03 15:01:18 -0300

    adicionar extensao jsx na importacao

--- front-end/src/pages/FinalSimulation.jsx
+++ front-end/src/pages/FinalSimulation.jsx
@@ -6,7 +6,7 @@ import PageTitle from "../components/PageTitle";
 
 import { formatBRL } from "../utils/currency";
 
-import PdfGenerator from "../components/PdfGenerator";
+import PdfGenerator from "../components/PdfGenerator.jsx";
 
 import {
   getMarginOptions,
